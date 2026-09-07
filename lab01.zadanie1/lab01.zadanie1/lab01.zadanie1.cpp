3b2045b5c7eb8ebf6c91ef1c07aa8e8a16c420a7
zetnert5051 <ivanzgon92017@gmail.com>
2026-09-07 12:20:03 +0300

    commit

---
+++ lab01.zadanie1/lab01.zadanie1/lab01.zadanie1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a;
+    std::cout << "Enter number: " << std::endl;
+    std::cin >> a;
+    int b = a * a;
+    std::cout << "Square of number: " << b;
+    return 0;
+}
---
+++ lab01.zadanie2/lab01.zadanie2/lab01.zadanie2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a;
+    std::cout << "Enter number: " << std::endl;
+    std::cin >> a;
+
+    int b = a * a;
+    int c = a * a * a;
+    std::cout << "Square of number: " << b << std::endl;
+    std::cout << "Cube of number: " << c;
+
+    return 0;
+}
